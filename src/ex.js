d79cc0f4f4393604b3264b99e5358bea08aa8009
marven mathelier <marven250@hotmail.com>
2020-05-22 12:56:10 -0400

    port # change

--- src/ex.js
+++
@@ -1,5 +0,0 @@
-let ok = new Date();
-console.log(ok.getMonth());
-const moment = require("moment");
-let um = moment().format("MMMM Do YYYY, h:mm:ss a");
-console.log(um);
--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import { reimbursementRouter } from "./Routers/reimbursementRoutes";
 import { findUserByUsernamePassword } from "./Database/user-data-access";
 import { sessionMiddleware } from "./Middleware/sessionMiddleware";
 const app: Application = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
