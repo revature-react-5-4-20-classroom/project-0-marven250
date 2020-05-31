ab6a9b26654f1ae96fa353c06971d27e301e8326
marven mathelier <marven250@hotmail.com>
2020-05-31 09:27:45 -0400

    modified cors again

--- src/Middleware/corsMiddleWare.ts
+++ src/Middleware/corsMiddleWare.ts
@@ -5,10 +5,5 @@ export function corsFilter(req: Request, res: Response, next: NextFunction) {
   res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
-  if (req.method === "OPTIONS") {
-    // This is the response to CORS preflight
-    next();
-  } else {
-    next();
-  }
+  next();
 }
